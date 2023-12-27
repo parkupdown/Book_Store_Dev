cdbf6e4b6a48e3709e9ea8fd7c022a7e4dd34e99
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-12-28 00:25:55 +0900

    feat: join, login API에 MariaDB 적용

--- user/routes/join.js
+++ user/routes/join.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+const connection = require("../../db/mariaDB.js");
+const conn = connection;
 const router = express.Router();
 // express가 제공하는 Router로 이것은 미들웨어가 된다.
-
 router.use(express.json());
 // express.json이라는 미들웨어
 // 따로 설정을 해주지 않는 한 모든 요청은 json으로 파싱될거다.
@@ -11,7 +11,25 @@ router.post("/", (req, res) => {
   let { email, password } = req.body;
   password = parseInt(password);
   //여기서 db연결에서 query날리기
-  res.json({ email: email, password: password });
+  conn.query(
+    "SELECT * FROM users WHERE email = ?",
+    email,
+    function (err, result) {
+      // 정상적으로 db가져옴 이제 만약 Db에 없으면 넣는걸로
+      if (result.length === 0) {
+        //없는거
+        conn.query(
+          "INSERT INTO users (email,password) VALUES (?,?)",
+          [email, password],
+          function (err, result) {
+            res.status(201).end();
+          }
+        );
+        return;
+      }
+      res.status(400).end();
+    }
+  );
 });
 
 module.exports = router;
--- user/routes/login.js
+++ user/routes/login.js
@@ -1,16 +1,31 @@
 const express = require("express");
-
+const connection = require("../../db/mariaDB");
+const conn = connection;
 const router = express.Router();
 // 미들웨어 router 생성
 router.use(express.json());
 
 router.post("/", (req, res) => {
   let { email, password } = req.body;
-  //여기서 DB랑 비교 후
-  //cookie로 JWT토큰 발행
   password = parseInt(password);
+  // 여기서 DB랑 비교 후
+  conn.query(
+    "SELECT * FROM users WHERE email = ? AND password = ?",
+    [email, password],
+    function (err, result) {
+      console.log(result);
+      if (result.length === 1) {
+        //있으면
+        res.status(200).end();
+        return;
+      }
+      res.status(400).end();
+    }
+  );
 
-  res.json({ email: email, password: password, message: "hi" });
+  // 타당성 검사도 넣어줘야함
+
+  //cookie로 JWT토큰 발행
 });
 
 module.exports = router;
