74428a26d08170494fe2fe9c51b81fa0b422345e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-01-03 20:17:25 +0900

    init (처음부터다시)

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 const express = require("express");
 const app = express();
 
-const joinRouter = require("./user/routes/join.js");
-const loginRouter = require("./user/routes/login.js");
-// Express는 미들웨어를 사용할 수 있다는게 큰 장점
-app.use("/join", joinRouter);
-app.use("/login", loginRouter);
-
-app.listen(2346);
+app.listen(3000);
