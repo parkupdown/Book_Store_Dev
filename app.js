fa350699b195f4eaabce45b572ecbd059abdbeae
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-01-03 20:51:36 +0900

    feat: 유저 API 초기 틀 개발

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // express 모듈
+const dotenv = require("dotenv");
+const router = require("./routes/users.js");
 
-app.listen(3000);
+const app = express();
+dotenv.config();
+app.listen(process.env.PORT, () => console.log("연결완료"));
+
+app.use(express.json());
+
+const userRouter = router;
+app.use("/users", userRouter);
