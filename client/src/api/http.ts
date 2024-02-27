1255c3899e68de238b603a99050c519970499754
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-02-27 21:43:54 +0900

    feat: Category API 개발

---
+++ client/src/api/http.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const BASE_URL = "http://localhost:9999";
+const DEFAULT_TIMEOUT = 30000;
+
+export const createClient = (config?: AxiosRequestConfig) => {
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: DEFAULT_TIMEOUT,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+    ...config,
+  });
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  return axiosInstance;
+};
+export const httpClient = createClient();
