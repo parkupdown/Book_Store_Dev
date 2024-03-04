873f04615af8b4f0f77abfadbc1e0213add74df4
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-03-04 17:57:31 +0900

    refactor: Order API 비동기 처리 (async await)

--- server/controller/OrderController.js
+++ server/controller/OrderController.js
@@ -1,45 +1,50 @@
 const conn = require("../mariadb"); // db 모듈
 const { StatusCodes } = require("http-status-codes"); //http 상태코드 모듈
 
-const order = (req, res) => {
+const order = async (req, res) => {
   const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
     req.body;
   let delivery_id;
   let order_id;
   let sql = "INSERT INTO delivery (address, receiver,contact) VALUES (?,?,?)";
   let values = [delivery.address, delivery.receiver, delivery.contact];
-  conn.query(sql, values, (err, result) => {
-    if (err) {
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    delivery_id = result.insertId;
-
-    sql =
-      "INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)";
-    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-
-    conn.query(sql, values, (err, result) => {
-      if (err) {
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      order_id = result.insertId;
-
-      sql = "INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?";
-      // items.. 배열 : 요소들을 하나씩 꺼내서 foreach문을 돌림
-      values = [];
-      items.forEach((item) => {
-        values.push([order_id, item.book_id, item.quantity]);
-      });
-
-      // 이렇게 하면 여러번의 query를 보내지 않아도 이중배열을 이용해서 여러번의 데이터를 한번에 넣어줄 수 있다.
-      conn.query(sql, [values], (err, result) => {
+  // 비동기함수니까 처리해줘야지
+  const getResult = (sql, values) => {
+    return new Promise((resolve, reject) => {
+      conn.query(sql, values, (err, result) => {
+        console.log(sql, values, err, result);
         if (err) {
           return res.status(StatusCodes.BAD_REQUEST).end();
         }
-        return res.status(StatusCodes.OK).json(result);
+
+        resolve(result);
       });
     });
+  };
+  // values가 이중배열로 들어가는데 conn.query에서 []로 받지않아서문제
+
+  const insertDeliveryResult = await getResult(sql, values);
+  delivery_id = insertDeliveryResult.insertId;
+
+  sql =
+    "INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)";
+  values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+
+  const insertOrderResult = await getResult(sql, values);
+  order_id = insertOrderResult.insertId;
+
+  sql = "INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?";
+  // items.. 배열 : 요소들을 하나씩 꺼내서 foreach문을 돌림
+  values = [];
+  items.forEach((item) => {
+    values.push([order_id, item.book_id, item.quantity]);
   });
+  console.log(values);
+  // 이렇게 하면 여러번의 query를 보내지 않아도 이중배열을 이용해서 여러번의 데이터를 한번에 넣어줄 수 있다.
+
+  const insertOrderedBookResult = await getResult(sql, [values]);
+
+  return res.status(StatusCodes.OK).json(insertOrderedBookResult);
 };
 
 const getOrders = () => {
